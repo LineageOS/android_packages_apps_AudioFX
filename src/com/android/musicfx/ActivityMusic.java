c8e8c87f21e5916ab5b0d3115e03ad1b317e2a70
wjiang <wjiang@codeaurora.org>
2014-01-02 12:00:00 +0800

    MusicFX: Fix virtualizer knob shows enabled by default.
    proxy uuid is added to support both h/w and s/w virtualizer effects.
    Add check condition to solve app behaviour abnormality.
    
    Change-Id: I216294ca324163a9a8d846425006c6909a0996ef
    CRs-Fixed: 594509

--- src/com/android/musicfx/ActivityMusic.java
+++ src/com/android/musicfx/ActivityMusic.java
@@ -221,8 +221,8 @@ public class ActivityMusic extends Activity {
             if (effect.type.equals(AudioEffect.EFFECT_TYPE_VIRTUALIZER)) {
                 mVirtualizerSupported = true;
                 if (effect.uuid.equals(UUID.fromString("1d4033c0-8557-11df-9f2d-0002a5d5c51b"))
-                        || effect.uuid.equals(UUID.fromString(
-                                "e6c98a16-22a3-11e2-b87b-f23c91aec05e"))) {
+                    || effect.uuid.equals(UUID.fromString("e6c98a16-22a3-11e2-b87b-f23c91aec05e"))
+                    || effect.uuid.equals(UUID.fromString("d3467faa-acc7-4d34-acaf-0002a5d5c51b"))) {
                     mVirtualizerIsHeadphoneOnly = true;
                 }
             } else if (effect.type.equals(AudioEffect.EFFECT_TYPE_BASS_BOOST)) {
