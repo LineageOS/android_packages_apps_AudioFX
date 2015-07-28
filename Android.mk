45abdd6c54eb78cdf5f2a1ff7e9492650a5f5fb0
Roman Birg <roman@cyngn.com>
2015-07-28 14:50:11 -0700

    AudioFX: sign with cyngn-app key
    Also clean up unused make file entries
    
    Ref: CYNGNOS-627
    Change-Id: I8538308dbf69459986b103a53cff5bb7e10ecfe1
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- Android.mk
+++ Android.mk
@@ -5,9 +5,16 @@ LOCAL_PACKAGE_NAME	:= AudioFX
 LOCAL_MODULE_TAGS	:= optional
 
 LOCAL_OVERRIDES_PACKAGES := DSPManager
-LOCAL_STATIC_JAVA_LIBRARIES := android-visualizer android-support-v13
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_JAVA_LIBRARIES := framework
+
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+
 LOCAL_PRIVILEGED_MODULE := true
+
+# Sign the package when not using test-keys
+ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/target/product/security/testkey)
+LOCAL_CERTIFICATE := cyngn-app
+endif
+
 include $(BUILD_PACKAGE)
