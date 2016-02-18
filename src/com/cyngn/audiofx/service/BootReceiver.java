75c8e499b830f97328c1c5005a333cdd632b753d
Roman Birg <roman@cyngn.com>
2016-02-18 07:36:26 -0800

    AudioFX: don't set defaults twice on first boot
    Ref: OPO-512
    
    Change-Id: Id6f10d38007983adeb5f4603c79a8116d1f95f1d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/cyngn/audiofx/service/BootReceiver.java
+++ src/com/cyngn/audiofx/service/BootReceiver.java
@@ -8,12 +8,18 @@ import com.cyngn.audiofx.Constants;
 
 public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        // reappply defaults on boot
-        SharedPreferences prefs = context.getSharedPreferences(Constants.AUDIOFX_GLOBAL_FILE, 0);
-        prefs.edit().putBoolean(Constants.SAVED_DEFAULTS, false).commit();
-
         final Intent service = new Intent(context, AudioFxService.class);
-        service.setAction(AudioFxService.ACTION_REAPPLY_DEFAULTS);
+
+        SharedPreferences prefs = context.getSharedPreferences(Constants.AUDIOFX_GLOBAL_FILE, 0);
+
+        if (prefs.contains(Constants.SAVED_DEFAULTS)) {
+            // only reset if we've already set this before - a reboot.
+            // we don't want to run apply defaults on the first boot, since the service
+            // will do the same thing the first time.
+            prefs.edit().putBoolean(Constants.SAVED_DEFAULTS, false).commit();
+
+            service.setAction(AudioFxService.ACTION_REAPPLY_DEFAULTS);
+        }
         context.startService(service);
     }
 }
