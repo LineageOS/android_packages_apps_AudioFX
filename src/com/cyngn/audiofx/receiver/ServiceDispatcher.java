7efd954d8ca805db56630a6ccd66c08138ffaa40
Roman Birg <roman@cyngn.com>
2016-02-29 18:53:59 +0000

    Revert "AudioFX: remove new session middle man"
    Ref: OPO-530
    
    This reverts commit 2c52f2a77f5c23276e8c717f90ec1afecc763f42.
    
    Change-Id: I70f76da8d4f37c09761166cb7fa2549511047224

---
+++ src/com/cyngn/audiofx/receiver/ServiceDispatcher.java
@@ -0,0 +1,35 @@
+package com.cyngn.audiofx.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.audiofx.AudioEffect;
+import android.util.Log;
+
+import com.cyngn.audiofx.service.AudioFxService;
+
+public class ServiceDispatcher extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent service = new Intent(context, AudioFxService.class);
+        String action = intent.getAction();
+
+        // We can also get AUDIO_BECOMING_NOISY, which means a device change is
+        // coming and we should wake up to handle it.
+        if (action.equals(AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION) ||
+                action.equals(AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION)) {
+            int sessionId = intent.getIntExtra(AudioEffect.EXTRA_AUDIO_SESSION, 0);
+            String pkg = intent.getStringExtra(AudioEffect.EXTRA_PACKAGE_NAME);
+            service.putExtra(AudioEffect.EXTRA_AUDIO_SESSION, sessionId);
+            service.putExtra(AudioEffect.EXTRA_PACKAGE_NAME, pkg);
+        }
+
+        service.setAction(action);
+        context.startService(service);
+        if (AudioFxService.DEBUG) {
+            Log.d("AudioFX-Dispatcher", "Received " + action);
+        }
+
+    }
+}
