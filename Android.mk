e4e3997e2d7df91be766c7031f5729bded78cdf4
Steve Kondik <shade@chemlab.org>
2016-04-08 12:54:04 -0700

    audiofx: Don't link with android-visualizer
     * This hasn't been used in a long time. Remove it.
    
    Change-Id: Icae39f301b3b2f2feca8a3517f7cb15ec300afe1

--- Android.mk
+++ Android.mk
@@ -3,15 +3,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-visualizer
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := framework
 
 LOCAL_OVERRIDES_PACKAGES := DSPManager
 
-LOCAL_PACKAGE_NAME := AudioFX
+LOCAL_PACKAGE_NAME := OSSAudioFX
 
 LOCAL_PRIVILEGED_MODULE := true
 
