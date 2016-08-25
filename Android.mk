e95b0267e571f128d881b9c0881f5e25f725239e
Roman Birg <roman@cyngn.com>
2016-08-25 06:30:53 -0700

    Revert "Revert "AudioFX: rename module back to AudioFX""
    This reverts commit c7dc272e9b7dbddc1931c54ad50ae8a2d6679292.

--- Android.mk
+++ Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PACKAGE_NAME	:= ModioFX
+LOCAL_PACKAGE_NAME	:= AudioFX
 LOCAL_MODULE_TAGS	:= optional
 
-LOCAL_OVERRIDES_PACKAGES := DSPManager AudioFX
+LOCAL_OVERRIDES_PACKAGES := DSPManager
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
