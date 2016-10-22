303d952cd0de0ddefae00046b964f8ea260ad017
Michael W <baddaemon87@gmail.com>
2016-10-22 12:58:59 +0200

    AudioFX: Fix possible RuntimeException
    Seems like mSessionManager is not initialised in some cases,
    and the call to destroy() causes a RuntimeException.
    Wrap with a null check to prevent the failure.
    
    Change-Id: I4713e303f3223d60a6674a0a1096c7b7f37a9814
    Reference: BugDumps 13-20161014-20 L#3
    (cherry picked from commit 57e73c474580531513c016f76bb486190685173d)

--- src/org/cyanogenmod/audiofx/service/AudioFxService.java
+++ src/org/cyanogenmod/audiofx/service/AudioFxService.java
@@ -288,7 +288,9 @@ public class AudioFxService extends Service
         if (DEBUG) Log.i(TAG, "Stopping service.");
 
         mOutputListener.removeCallback(this, mSessionManager, mDevicePrefs);
-        mSessionManager.onDestroy();
+        if (mSessionManager != null) {
+            mSessionManager.onDestroy();
+        }
 
         CMStatusBarManager.getInstance(this).removeTile(TILE_ID);
 
