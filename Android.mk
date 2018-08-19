c71c96c7509f332d073a1af4513fa8b0a1606099
Bruno Martins <bgcngm@gmail.com>
2018-08-19 01:54:37 +0100

    AudioFX: Properly depend on Lineage SDK
     * Platform apps that depend on the SDK must only link
       against org.lineageos.platform.internal
    
    Change-Id: Ibf63433f0961572d118361f984270f56accf8409

--- Android.mk
+++ Android.mk
@@ -6,7 +6,9 @@ LOCAL_MODULE_TAGS	:= optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 org.lineageos.platform.sdk
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_ENABLED := disabled
 
