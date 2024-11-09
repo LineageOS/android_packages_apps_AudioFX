631d6939ed5b4797e39fab8f5b0856e7a5c6958d
Michael W <baddaemon87@gmail.com>
2024-11-09 21:29:46 +0100

    AudioFX: Use MaterialSwitch
    Change-Id: Id46003497ae9b167dd16dd4bc3776598aec24303

--- src/org/lineageos/audiofx/activity/ActivityMusic.java
+++ src/org/lineageos/audiofx/activity/ActivityMusic.java
@@ -29,6 +29,8 @@ import android.view.ViewStub;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.google.android.material.materialswitch.MaterialSwitch;
+
 import org.lineageos.audiofx.Constants;
 import org.lineageos.audiofx.R;
 import org.lineageos.audiofx.fragment.AudioFxFragment;
@@ -43,7 +45,7 @@ public class ActivityMusic extends Activity {
     public static final String TAG_AUDIOFX = "audiofx";
     public static final String EXTRA_CALLING_PACKAGE = "audiofx::extra_calling_package";
 
-    private Switch mCurrentDeviceToggle;
+    private MaterialSwitch mCurrentDeviceToggle;
     MasterConfigControl mConfig;
     String mCallingPackage;
 
